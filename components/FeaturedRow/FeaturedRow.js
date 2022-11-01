3bb2ff62f61120fcc1a8c4a8f4b12d27503e4ca0
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-11-01 20:10:32 +0600

    card-created-for each-part

--- components/FeaturedRow/FeaturedRow.js
+++ components/FeaturedRow/FeaturedRow.js
@@ -1,15 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import ResturantCard from '../ResturantCard/ResturantCard'
 
-const FeaturedRow = ({title,description,id}) => {
+const FeaturedRow = ({ title, description, id }) => {
   return (
     <View>
-          <View className="mt-4 flex-row items-center justify-between px-4">
-              <Text className="font-bold text-lg">{title}</Text>
-              <ArrowRightIcon color='cyan'></ArrowRightIcon>
-          </View>
-          <Text className="text-xs text-gray-500 px-4">{ description}</Text>
+      <View className='mt-4 flex-row items-center justify-between px-4'>
+        <Text className='font-bold text-lg'>{title}</Text>
+        <ArrowRightIcon color='cyan'></ArrowRightIcon>
+      </View>
+      <Text className='text-xs text-gray-500 px-4'>{description}</Text>
+
+      <ScrollView
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className='pt-4'
+      >
+        {/* Resturants card... */}
+        <ResturantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title='Yo!Susi'
+          rating={4.8}
+          address='Austin USA'
+          short_description='This is a test Card descripton'
+          dishes={[]}
+          long={20}
+          lat={4}
+          genere='Japanese'
+        ></ResturantCard>
+        <ResturantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title='Yo!Susi'
+          rating={4.8}
+          address='Austin USA'
+          short_description='This is a test Card descripton'
+          dishes={[]}
+          long={20}
+          lat={4}
+          genere='Japanese'
+        ></ResturantCard>
+        <ResturantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title='Yo!Susi'
+          rating={4.8}
+          address='Austin USA'
+          short_description='This is a test Card descripton'
+          dishes={[]}
+          long={20}
+          lat={4}
+          genere='Japanese'
+        ></ResturantCard>
+        <ResturantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title='Yo!Susi'
+          rating={4.8}
+          address='Austin USA'
+          short_description='This is a test Card descripton'
+          dishes={[]}
+          long={20}
+          lat={4}
+          genere='Japanese'
+        ></ResturantCard>
+      </ScrollView>
     </View>
   )
 }
